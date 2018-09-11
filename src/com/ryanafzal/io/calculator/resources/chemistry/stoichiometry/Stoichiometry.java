63198b135b74125dba030170c6d33cdf1306ad64
ryana <ryana@10.0.0.17>
2018-09-10 19:57:57 -0700

    Improved the documentation of Stoichiometry.java

--- src/com/ryanafzal/io/calculator/resources/chemistry/stoichiometry/Stoichiometry.java
+++ src/com/ryanafzal/io/calculator/resources/chemistry/stoichiometry/Stoichiometry.java
@@ -25,14 +25,43 @@ public class Stoichiometry {
 		this.startingValue = startingValue;
 	}
 	
+	
+	
+	/**
+	 * Solves the stoichiometry problem for the given {@code Chemical}, in the given {@code Unit}.
+	 * 
+	 * Process:
+	 * 
+	 * Convert {@code Prefix} to {@code Prefix.NONE}. 
+	 * 
+	 * Convert {@code Unit} to {@code QuantityUnit}.
+	 * 
+	 * Convert {@code QuantityUnit} to {@code MoleUnit} depending on the molar mass of the {@code Chemical}.
+	 * 
+	 * Perform the stoichiometric operation.
+	 * 
+	 * Convert {@code Unit} to desired output {@code Unit}.
+	 * 
+	 * @param chemical The {@code Chemical} to solve for
+	 * @param unit The {@code Unit} to return the value in
+	 * @return Returns a {@code RailRoad} containing the entire stoichiometric solution. This does not include other equations, such as Gas Laws.
+	 * 
+	 */
 	public RailRoad solveFor(Chemical chemical, Class<? extends Unit> unit) throws EquationException, InvalidUnitException {
 		RailRoad railroad = new RailRoad(this.startingValue);
 		ChemicalValue new_chemical = this.equation.getChemicalValue(chemical);
+		
+		
+		
 		
 		//TODO: Since the only acceptable units are grams and moles, throw an exception if anything else is passed.
 		if (!this.startingValue.getUnit().getClass().equals(MoleUnit.class) && !this.startingValue.getUnit().getClass().equals(QuantityUnit.class)) {
 			throw new IllegalArgumentException("Unit " + unit + " is not a valid unit.");
 		}
+		
+		
+		
+		
 		
 		//Handle conversion of prefixes PREFIX -> NONE
 		if (!this.startingValue.getUnit().getPrefix().equals(Prefix.NONE)) {
@@ -80,9 +109,9 @@ public class Stoichiometry {
 			}
 		}
 		
+		
+		
 		//Convert Unit to Grams
-		
-		
 		//If not in moles, convert units of value from grams to moles.
 		if (!(this.startingValue.getUnit() instanceof MoleUnit)) {
 			railroad.addComponent(
@@ -100,7 +129,8 @@ public class Stoichiometry {
 									this.startingValue.getState())));
 		}
 		
-		//Convert to new chemical
+		
+		//Perform stoichiometric operation
 		double[] conversion = this.equation.getRatioBetween(chemical, this.startingValue.getChemical());
 		railroad.addComponent(new RailRoadComponent(
 				new ChemicalValue(conversion[0], new MoleUnit(), chemical, new_chemical.getState()), 
@@ -137,6 +167,8 @@ public class Stoichiometry {
 			}
 		}
 		
+		
+		//Return the RailRoad (Dimensional Analysis) representing the stoichiometric solution.
 		return railroad;
 	}
 	
