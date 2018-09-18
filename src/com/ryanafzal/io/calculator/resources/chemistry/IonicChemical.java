77fe51ffa14930b80216e767b1db3d4643fda5e6
s-afzalr <s-afzalr@bsd405.org>
2018-09-17 21:23:47 -0700

    Work on Ionics

--- src/com/ryanafzal/io/calculator/resources/chemistry/IonicChemical.java
+++ src/com/ryanafzal/io/calculator/resources/chemistry/IonicChemical.java
@@ -8,15 +8,8 @@ public class IonicChemical implements IChemical {
 	private int num_cation;
 	private int num_anion;
 	
-	public IonicChemical(Chemical cation, int num_cation, Chemical anion, int num_anion) {
-		if (cation.getCharge() < 1 || anion.getCharge() > -1 || num_cation <= 0 || num_anion <= 0) {
-			throw new IllegalArgumentException();
-		}
-
-		this.cation = cation;
-		this.anion = anion;
-		this.num_cation = num_cation;
-		this.num_anion = num_anion;
+	public IonicChemical() {
+		
 	}
 	
 	@Override
