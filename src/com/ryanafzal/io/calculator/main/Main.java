409cc0afce1dc092f2c1e02d255014b53298f068
s-afzalr <s-afzalr@bsd405.org>
2018-09-26 09:28:42 -0700

    Almost finished Hydrocarbon Generation

--- src/com/ryanafzal/io/calculator/main/Main.java
+++ src/com/ryanafzal/io/calculator/main/Main.java
@@ -2,11 +2,27 @@ package com.ryanafzal.io.calculator.main;
 
 import javax.swing.*;
 
+import com.ryanafzal.io.calculator.resources.chemistry.structure.BondType;
+import com.ryanafzal.io.calculator.resources.chemistry.structure.Chemical;
+import com.ryanafzal.io.calculator.resources.chemistry.structure.constructs.StructuralException;
+import com.ryanafzal.io.calculator.resources.chemistry.structure.constructs.groups.Group;
+import com.ryanafzal.io.calculator.resources.chemistry.structure.constructs.groups.Hydrocarbon;
+
 @SuppressWarnings("unused")
 public class Main {
 	
 	public static void main(String[] args) {
-		
+		try {
+			
+			
+			Group propane = Hydrocarbon.getAliphaticHydrocarbon(2, false, new BondType[] {BondType.SINGLE});
+			propane.replaceRValues();
+			Chemical propane_chemical = propane.createChemicalFromGroup();
+			
+			System.out.println(propane_chemical.getLaTeXString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
--- src/com/ryanafzal/io/calculator/resources/chemistry/structure/constructs/groups/Hydrocarbon.java
+++ src/com/ryanafzal/io/calculator/resources/chemistry/structure/constructs/groups/Hydrocarbon.java
@@ -1,6 +1,9 @@
 package com.ryanafzal.io.calculator.resources.chemistry.structure.constructs.groups;
 
+import java.util.Arrays;
+
 import com.ryanafzal.io.calculator.resources.chemistry.structure.AtomType;
+import com.ryanafzal.io.calculator.resources.chemistry.structure.BondType;
 import com.ryanafzal.io.calculator.resources.chemistry.structure.constructs.AtomConstruct;
 import com.ryanafzal.io.calculator.resources.chemistry.structure.constructs.BondConstruct;
 import com.ryanafzal.io.calculator.resources.chemistry.structure.constructs.RConstruct;
@@ -8,13 +11,44 @@ import com.ryanafzal.io.calculator.resources.chemistry.structure.constructs.Subs
 
 public class Hydrocarbon extends Group {
 
-	public Hydrocarbon(SubstituentConstruct[] subs, BondConstruct[] bonds) {
+	private Hydrocarbon(SubstituentConstruct[] subs, BondConstruct[] bonds) {
 		super(subs, bonds);
 	}
 	
-	public static Hydrocarbon getAliphaticHydrocarbon(int num_carbon, boolean cyclic, int[] num_double, int[] num_triple) {
+	public static Hydrocarbon getAliphaticHydrocarbon(int num_carbon, boolean cyclic, BondType[] carbon_bond_pattern) {
 		
-		int num_r = ((num_carbon * 3) - (num_double.length * 2) - (num_triple.length * 3));
+		if (cyclic && num_carbon < 3) {
+			throw new IllegalArgumentException("A cyclic hydrocarbon must have at least 3 carbon atoms.");
+		}
+		
+		int num_double = 0;
+		int num_triple = 0;
+		
+		int expected_pattern_size;
+		if (cyclic) {
+			expected_pattern_size = num_carbon;
+		} else {
+			expected_pattern_size = num_carbon - 1;
+		}
+		
+		if (carbon_bond_pattern.length != expected_pattern_size) {
+			throw new IllegalArgumentException("The supplied bond pattern of " + Arrays.toString(carbon_bond_pattern) + " is not the correct length.");
+		}
+		
+		for (BondType i : carbon_bond_pattern) {
+			if (i == BondType.TRIPLE) {
+				num_triple++;
+			} else if (i == BondType.DOUBLE) {
+				num_double++;
+			} else if (i != BondType.SINGLE) {
+				throw new IllegalArgumentException("The supplied bond pattern of " + Arrays.toString(carbon_bond_pattern) + " is invalid.");
+			}
+		}
+		
+		int num_r = ((num_carbon * 2) - (num_double * 2) - (num_triple * 3));
+		if (!cyclic) {
+			num_r += 2;
+		}
 		SubstituentConstruct[] subs = new SubstituentConstruct[num_r + num_carbon];
 		
 		BondConstruct[] bonds;
@@ -30,13 +64,90 @@ public class Hydrocarbon extends Group {
 		for (int i = 0; i < num_carbon; i++) {
 			subs[i] = new AtomConstruct(AtomType.CARBON, -4);
 		}
+		
+		
 		for (int i = num_carbon; i < subs.length; i++) {
 			subs[i] = new RConstruct();
 		}
 		
 		
+		int start = num_carbon;
+		int bond = 0;
+		//Attach the carbon atoms to their corresponding values
+		for (int c = 0; c < num_carbon - 1; c++) {
+			int depth;
+			BondType bond_pattern = carbon_bond_pattern[c];
+			if (bond_pattern == BondType.TRIPLE) {
+				depth = 0;
+			} else if (bond_pattern == BondType.DOUBLE) {
+				depth = 1;
+			} else {
+				depth = 2;
+			}
+			
+			bonds[bond] = new BondConstruct(c, bond_pattern, c + 1);
+			bond++;
+			
+			for (int i = start; i < start + depth; i++) {
+				bonds[bond] = new BondConstruct(c, BondType.SINGLE, i);
+				bond++;
+			}
+			
+			start += depth;
+		}
 		
-		return null;
+		if (cyclic) {
+			int c = num_carbon - 1;
+			int depth;
+			BondType bond_pattern = carbon_bond_pattern[c];
+			if (bond_pattern == BondType.TRIPLE) {
+				depth = 0;
+			} else if (bond_pattern == BondType.DOUBLE) {
+				depth = 1;
+			} else {
+				depth = 2;
+			}
+			
+			bonds[bond] = new BondConstruct(c, bond_pattern, 0);
+			bond++;
+			
+			for (int i = start; i < start + depth; i++) {
+				bonds[bond] = new BondConstruct(c, BondType.SINGLE, i);
+				bond++;
+			}
+			
+			start += depth;
+		} else if (num_carbon == 1) {
+			for (int i = start; i < start + 2; i++) {
+				bonds[bond] = new BondConstruct(0, BondType.SINGLE, i);
+				bond++;
+			}
+			
+			bonds[bonds.length - 2] = new BondConstruct(0, BondType.SINGLE, subs.length - 2);
+			bonds[bonds.length - 1] = new BondConstruct(num_carbon - 1, BondType.SINGLE, subs.length - 1);
+		} else {
+			int c = num_carbon - 2;
+			int depth;
+			BondType bond_pattern = carbon_bond_pattern[c];
+			if (bond_pattern == BondType.TRIPLE) {
+				depth = 0;
+			} else if (bond_pattern == BondType.DOUBLE) {
+				depth = 1;
+			} else {
+				depth = 2;
+			}
+			
+			c++;
+			
+			for (int i = start; i < start + depth; i++) {
+				bonds[bond] = new BondConstruct(c, BondType.SINGLE, i);
+				bond++;
+			}
+			bonds[bonds.length - 2] = new BondConstruct(0, BondType.SINGLE, subs.length - 2);
+			bonds[bonds.length - 1] = new BondConstruct(num_carbon - 1, BondType.SINGLE, subs.length - 1);
+		}
+		
+		return new Hydrocarbon(subs, bonds);
 	}
 
 }
